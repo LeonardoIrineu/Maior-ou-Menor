017e8fd51152e768fb5ad0e149228fd3282dbe52
Leonardo Irineu <leo_izidoro_@hotmail.com>
2022-09-29 10:11:04 -0300

    função par ou impar

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let n2 = document.getElementById('numero2')
 let n3 = Number(n1.value)
 let n4 = Number(n2.value)
 let resultado = document.getElementById('resultado')
+let res = document.querySelector('#res')
+let res1 = document.querySelector('#res1')
 
     if (n3 > n4) {        
         resultado.innerHTML = `\u{2705}${n3} é maior que ${n4}\u{2705}`
@@ -11,6 +13,14 @@ let resultado = document.getElementById('resultado')
         resultado.innerHTML = `\u{2705}${n4} é maior que ${n3}\u{2705}`
     } if (n3 == n4) {
         resultado.innerHTML = `\u{2705}${n3} é igual a ${n4}\u{2705}`
-    } 
+    } if (n3 % 2 === 0) {
+        res.innerHTML = `${n3} é Par`
+    } else if (n3 % 2 !== 0){
+        res.innerHTML = `${n3} é Impar`
+    } if (n4 % 2 === 0) {
+        res1.innerHTML = `${n4} é Par`
+    } else if (n4 % 2 !== 0) {
+        res1.innerHTML = `${n4} é Impar`
+    }
 }
 
